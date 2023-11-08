e3b2479c12fa97dc8ac30e042e8f41ad4e0e42a8
ahiyantra <45235557+ahiyantra@users.noreply.github.com>
2023-11-08 10:08:05 +0530

    project changes #01

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,12 +1,13 @@
 ''' Module for Emotion Detection package: emotion_detection.py
 '''
+'''
 import json
 import requests
-import watson_nlp
+import ibm_watson # import watson_nlp
 
 def emotion_detector(text_to_analyse):
-    ''' Function to run emotion detection of user text input 
-    '''
+    # Function to run emotion detection of user text input 
+    
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": {"text": text_to_analyse}}
@@ -40,3 +41,56 @@ def emotion_detector(text_to_analyse):
         return None
 
     # return syntax_prediction
+'''
+
+import os
+import json
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve service credentials from environment variables
+api_key = os.getenv("IBM_WATSON_API_KEY")
+service_url = os.getenv("IBM_WATSON_SERVICE_URL")
+
+# Initialize the IBM Watson Natural Language Understanding service
+authenticator = IAMAuthenticator(api_key)
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version="2021-08-01",
+    authenticator=authenticator
+)
+natural_language_understanding.set_service_url(service_url)
+
+def emotion_detector(text_to_analyze):
+    try:
+        response = natural_language_understanding.analyze(
+            text=text_to_analyze,
+            features={"emotion": {}}
+        ).get_result()
+
+        if response is not None:
+            emotions = response.get("emotion", {}).get("document", {}).get("emotion", {})
+            dominant_emotion = max(emotions, key=emotions.get)
+
+            return {
+                "anger": emotions.get("anger"),
+                "disgust": emotions.get("disgust"),
+                "fear": emotions.get("fear"),
+                "joy": emotions.get("joy"),
+                "sadness": emotions.get("sadness"),
+                "dominant_emotion": dominant_emotion
+            }
+        else:
+            return None
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
+
+# Example usage
+text_to_analyze = "I am feeling very happy today!"
+result = emotion_detector(text_to_analyze)
+print(result)
